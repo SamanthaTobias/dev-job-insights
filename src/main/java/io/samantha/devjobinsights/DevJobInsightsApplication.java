abbf1deca08e8f45e332b9eaeef1489212ba12f0
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-05-06 16:51:41 +0200

    initial commit

---
+++ src/main/java/io/samantha/devjobinsights/DevJobInsightsApplication.java
@@ -0,0 +1,13 @@
+package io.samantha.devjobinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJobInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJobInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/samantha/devjobinsights/DevJobInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.samantha.devjobinsights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
